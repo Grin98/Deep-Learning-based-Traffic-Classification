066f3a6888e4594a65490cccd88b74e7e0fcbfb7
Grin98 <norgrin123@gmail.com>
2020-05-11 09:25:21 +0300

    refactored SplitPreProcessor

--- flowpic_dataset/processor.py
+++ flowpic_dataset/processor.py
@@ -38,14 +38,14 @@ class BaseProcessor(ABC):
     def _process_dir_files(self, input_dir_path: Path):
         pass
 
-    def __process_flows__(self, flows):
+    def _process_flows(self, flows):
         blocks = []
         for app, sizes, times in flows:
-            blocks += self.__split_flow_to_blocks__(times, sizes)
+            blocks += self._split_flow_to_blocks(times, sizes)
 
         return blocks
 
-    def __transform_row_to_flow__(self, row: List[str]) -> Tuple:
+    def _transform_row_to_flow(self, row: List[str]) -> Tuple:
         app = row[0]
         num_packets = int(row[7])
         off_set = 8  # meta data occupies first 8 indices
@@ -62,7 +62,7 @@ class BaseProcessor(ABC):
 
         return app, sizes, times
 
-    def __split_flow_to_blocks__(self, times, sizes):
+    def _split_flow_to_blocks(self, times, sizes):
         num_blocks = int(times[-1] / self.block_delta - self.block_duration / self.block_delta) + 1
         blocks = []
         for b in range(num_blocks):
@@ -84,7 +84,7 @@ class BaseProcessor(ABC):
         return blocks
 
     @staticmethod
-    def __create_dir__(dir: Path):
+    def _create_dir(dir: Path):
         if not dir.exists():
             dir.mkdir(parents=True, exist_ok=True)
 
@@ -95,7 +95,7 @@ class BaseProcessor(ABC):
     def _create_output_dir(self, out_root_dir: Path, input_dir_path: Path):
         sub_path = Path(*input_dir_path.parts[1:])
         out_path_dir = out_root_dir / sub_path
-        self.__create_dir__(out_path_dir)
+        self._create_dir(out_path_dir)
         return out_path_dir
 
 
@@ -105,69 +105,76 @@ class SplitPreProcessor(BaseProcessor):
     and in addition there is an overlap between consecutive blocks depending on the value of [block_delta_in_seconds]
     """
 
-    def __init__(self, test_dir, train_dir, block_duration_in_seconds: int = 60,
+    def __init__(self, train_dir, test_dir,
+                 test_percent: float = 0.1,
+                 block_duration_in_seconds: int = 60,
                  block_delta_in_seconds: int = 15, packet_size_limit: int = 1500):
         super().__init__(block_duration_in_seconds, block_delta_in_seconds, packet_size_limit)
-        self.testPath = Path(test_dir)
         self.trainPath = Path(train_dir)
+        self.testPath = Path(test_dir)
+        self.test_percent = test_percent
 
     def _process_dir_files(self, input_dir_path: Path):
         out_file = 'data.csv'
         test_file_path = self._create_output_dir(self.testPath, input_dir_path) / out_file
         train_file_path = self._create_output_dir(self.trainPath, input_dir_path) / out_file
 
+        print('processing %s' % input_dir_path)
         with test_file_path.open('w+', newline='') as test_out:
             test_writer = csv.writer(test_out, delimiter=',')
 
             with train_file_path.open('w+', newline='') as train_out:
                 train_writer = csv.writer(train_out, delimiter=',')
 
-                test_blocks = []
-                train_blocks = []
+                flows = []
                 for file in self._get_dir_items(input_dir_path):
-                    file_test_blocks, file_train_blocks = self._process_file(file)
-                    test_blocks += file_test_blocks
-                    train_blocks += file_train_blocks
+                    flows += self._process_file(file)
 
-                test_blocks += train_blocks
-                self.__sample_and_write_blocks__(test_blocks, test_writer, target_amount=4000)
-                # self.__sample_and_write_blocks__(train_blocks, train_writer, approximate_amount=3000)
+                train_flows, test_flows = self._split_train_test(flows, self.test_percent)
+                train_blocks = self._process_flows(train_flows)
+                test_blocks = self._process_flows(test_flows)
+
+                train_blocks = self._sample_blocks(train_blocks, target_amount=4000)
+                test_blocks = self._sample_blocks(test_blocks, target_amount=300)
+
+                self._write_blocks(train_blocks, train_writer)
+                self._write_blocks(test_blocks, test_writer)
 
     def _process_file(self, input_file_path: Path):
-
         file_extension = input_file_path.suffix
         if file_extension != '.csv':
-            return [], []
-
-        print('processing ' + str(input_file_path))
+            return []
 
         with input_file_path.open(newline='') as f_in:
             data = csv.reader(f_in, delimiter=',')
-
-            flows = [self.__transform_row_to_flow__(row) for row in data]
-            num_flows = len(flows)
-
-            # create ndarray of tuples
-            train_flows = np.empty(num_flows, dtype=object)
-            train_flows[:] = flows
-
-            # split
-            test_indices = random.sample(range(num_flows), max(1, int(num_flows * 0.1)))
-            test_flows = train_flows[test_indices]
-            train_flows = np.delete(train_flows, test_indices)
-
-            test_blocks = self.__process_flows__(test_flows)
-            train_blocks = self.__process_flows__(train_flows)
-
-            return test_blocks, train_blocks
+            return [self._transform_row_to_flow(row) for row in data]
 
     @staticmethod
-    def __sample_and_write_blocks__(blocks, writer, target_amount: int):
+    def _split_train_test(flows, test_percent):
+        num_flows = len(flows)
+
+        # create ndarray of tuples
+        train_flows = np.empty(num_flows, dtype=object)
+        train_flows[:] = flows
+
+        # split
+        test_indices = random.sample(range(num_flows), max(1, int(num_flows * test_percent)))
+        test_flows = train_flows[test_indices]
+        train_flows = np.delete(train_flows, test_indices)
+
+        return train_flows, test_flows
+
+    @staticmethod
+    def _sample_blocks(blocks, target_amount: int):
         current_amount = len(blocks)
         if current_amount > target_amount:
             indices = random.sample(range(current_amount), target_amount)
             blocks = np.array(blocks)[indices]
 
+        return blocks
+
+    @staticmethod
+    def _write_blocks(blocks, writer):
         for b in blocks:
             writer.writerow(b)
 
@@ -201,8 +208,8 @@ class NoOverlapPreProcessor(BaseProcessor):
             data = csv.reader(f_in, delimiter=',')
 
             for row in data:
-                app, sizes, times = self.__transform_row_to_flow__(row)
-                blocks = self.__split_flow_to_blocks__(times, sizes)
+                app, sizes, times = self._transform_row_to_flow(row)
+                blocks = self._split_flow_to_blocks(times, sizes)
                 for b in blocks:
                     writer.writerow(b)
 
@@ -210,12 +217,14 @@ class NoOverlapPreProcessor(BaseProcessor):
 class StatisticsProcessor(BaseProcessor):
 
     def __init__(self, out_root_dir_path: str,
+                 is_raw_data: bool = True,
                  block_duration_in_seconds: int = 60,
                  block_delta_in_seconds: int = 15,
                  packet_size_limit: int = 1500):
 
         super().__init__(block_duration_in_seconds, block_delta_in_seconds, packet_size_limit)
         self.out_root_dir = Path(out_root_dir_path)
+        self.is_raw_data = is_raw_data
 
     def _process_dir_files(self, input_dir_path: Path):
         out_dir_path = self._create_output_dir(self.out_root_dir, input_dir_path)
@@ -254,8 +263,11 @@ class StatisticsProcessor(BaseProcessor):
 
             blocks = []
             for row in data:
-                app, sizes, times = self.__transform_row_to_flow__(row)
-                blocks += self.__split_flow_to_blocks__(times, sizes)
+                if self.is_raw_data:
+                    app, sizes, times = self._transform_row_to_flow(row)
+                    blocks += self._split_flow_to_blocks(times, sizes)
+                else:
+                    blocks += row
 
             return list(map(self._get_block_meta, blocks))
 
