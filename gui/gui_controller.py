2741dfb0614200a6b138e86bb8d8c5a8cc18ffc5
gilzim <gilzi@campus.technion.ac.il>
2020-05-19 18:30:34 +0300

    Changed pcap classification graph style and added threading

--- gui/gui_controller.py
+++ gui/gui_controller.py
@@ -2,12 +2,15 @@ from tkinter import ttk
 from tkinter import *
 from tkinter import filedialog
 import matplotlib
+import threading
+import queue
 
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 LARGE_FONT = ("Verdana", 12)
+result_queue = queue.Queue()
 
 
 class GuiController(Tk):
@@ -54,36 +57,73 @@ class PcapClassificationPage(ttk.Frame):
         ttk.Frame.__init__(self, parent, padding=(12, 12, 12, 12))
 
         self.pcap_file_path = None
-        self.pcap_file_name = StringVar(self, "Pcap Classification")
-
-        title_label = ttk.Label(self, font=LARGE_FONT, anchor="center", textvariable=self.pcap_file_name)
-
-        f = Figure(figsize=(5, 5), dpi=100)
-        a = f.add_subplot(111)
-        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
-
-        graph = FigureCanvasTkAgg(f, self)
-
-        pcap_button = ttk.Button(self, text="Upload pcap file", width=24, command=self.upload_pcap_file)
-        back_button = ttk.Button(self, width=24, text="Back",
-                                 command=lambda: controller.show_frame(StartPage))
+        self.pcap_file_name = ""
+        self.pcap_file_label_variable = StringVar(self)
+        self.controller = controller
 
         progress_bar_frame = ttk.Frame(self, padding=(5, 5, 5, 5), width=200)
         progress_bar_frame.grid(column=2, row=5, columnspan=2)
         self.progress_bar = ttk.Progressbar(progress_bar_frame, orient=HORIZONTAL, length=200,
                                             mode='indeterminate')
+        self.pcap_label = ttk.Label(progress_bar_frame, font=LARGE_FONT, anchor="center",
+                                    textvariable=self.pcap_file_label_variable)
 
-        graph._tkcanvas.grid(column=1, row=1, columnspan=4, rowspan=4)
-        pcap_button.grid(column=4, row=6, columnspan=2)
-        back_button.grid(column=0, row=6, columnspan=2)
+        self.figure = Figure(figsize=(6, 6), dpi=100)
+        self.graph = FigureCanvasTkAgg(self.figure, self)
+        self.graph._tkcanvas.grid(column=1, row=1, columnspan=4, rowspan=4)
+
+        title_label = ttk.Label(self, font=LARGE_FONT, anchor="center", text="Pcap File Classification")
+
+        pcap_button = ttk.Button(self, text="Upload pcap file", width=24, command=self.upload_pcap_file)
+        back_button = ttk.Button(self, width=24, text="Back",
+                                 command=lambda: self.on_back_button_click())
+
+        pcap_button.grid(column=4, row=7, columnspan=2)
+        back_button.grid(column=0, row=7, columnspan=2)
         title_label.grid(column=2, row=0, columnspan=2)
 
     def upload_pcap_file(self):
+        # Open pcap file and save its location and name
         self.pcap_file_path = filedialog.askopenfilename()
-        self.pcap_file_name.set(self.pcap_file_path.split("/")[-1])
-        self.progress_bar.grid(column=2, row=5, columnspan=2)
-
+        self.pcap_file_name = self.pcap_file_path.split("/")[-1]
+        self.pcap_file_label_variable.set("Classifying " + self.pcap_file_name)
+        # Show Classifying animation
+        self.pcap_label.grid(column=2, row=5, columnspan=2)
+        self.progress_bar.grid(column=2, row=6, columnspan=2)
         self.progress_bar.start(15)
+        # Begin classifying process
+        threading.Thread(target=lambda: self.classify_pcap_file()).start()
+
+        self.master.after(100, self.check_classify_progress)
+
+    def check_classify_progress(self):
+        try:
+            names, values = result_queue.get()
+            self.progress_bar.grid_forget()
+            self.pcap_label.grid_forget()
+            self.create_graph(names, values)
+        except queue.Empty:
+            pass
+
+    def create_graph(self, names, values):
+        p = self.figure.add_subplot(111)
+        self.figure.suptitle(self.pcap_file_name)
+        p.bar(names, values)
+        self.graph.draw()
+
+    def classify_pcap_file(self):
+        # TODO change to actual classification
+        names = ['VoIP', 'Video', 'File Transfer', 'Chat', "Browsing"]
+        values = [43, 22, 15, 93, 85]
+        result_queue.put((names, values))
+
+    def on_back_button_click(self):
+        self.progress_bar.stop()
+        self.progress_bar.grid_forget()
+        self.pcap_label.grid_forget()
+        self.figure.clear()
+        self.graph.draw()
+        self.controller.show_frame(StartPage)
 
 
 class StartPage(ttk.Frame):
