cedf3d90895fde25e9ec974f031095830b9a5d25
gilzim <gilzi@campus.technion.ac.il>
2020-05-26 15:16:42 +0300

    Changed Graphs layout

--- gui/gui_controller.py
+++ gui/gui_controller.py
@@ -8,6 +8,7 @@ import queue
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 LARGE_FONT = ("Verdana", 12)
 result_queue = queue.Queue()
@@ -59,28 +60,33 @@ class PcapClassificationPage(ttk.Frame):
         self.pcap_file_path = None
         self.pcap_file_name = ""
         self.pcap_file_label_variable = StringVar(self)
+        self.pcap_values = []
+        self.pcap_lables = []
         self.controller = controller
+        self.slider_graph = None
 
         progress_bar_frame = ttk.Frame(self, padding=(5, 5, 5, 5), width=200)
-        progress_bar_frame.grid(column=2, row=5, columnspan=2)
         self.progress_bar = ttk.Progressbar(progress_bar_frame, orient=HORIZONTAL, length=200,
                                             mode='indeterminate')
         self.pcap_label = ttk.Label(progress_bar_frame, font=LARGE_FONT, anchor="center",
                                     textvariable=self.pcap_file_label_variable)
 
-        self.figure = Figure(figsize=(6, 6), dpi=100)
+        self.figure = plt.figure(figsize=(8, 8), dpi=100)
         self.graph = FigureCanvasTkAgg(self.figure, self)
         self.graph._tkcanvas.grid(column=1, row=1, columnspan=4, rowspan=4)
 
+        self.slider = Scale(self, length=200, from_=60, to=300, orient=HORIZONTAL, resolution=60,
+                            command=self.on_slider_change)
         title_label = ttk.Label(self, font=LARGE_FONT, anchor="center", text="Pcap File Classification")
 
         pcap_button = ttk.Button(self, text="Upload pcap file", width=24, command=self.upload_pcap_file)
         back_button = ttk.Button(self, width=24, text="Back",
                                  command=lambda: self.on_back_button_click())
 
-        pcap_button.grid(column=4, row=7, columnspan=2)
-        back_button.grid(column=0, row=7, columnspan=2)
+        pcap_button.grid(column=4, row=8, columnspan=2)
+        back_button.grid(column=0, row=8, columnspan=2)
         title_label.grid(column=2, row=0, columnspan=2)
+        progress_bar_frame.grid(column=2, row=6, columnspan=2)
 
     def upload_pcap_file(self):
         self.figure.clear()
@@ -89,8 +95,8 @@ class PcapClassificationPage(ttk.Frame):
         self.pcap_file_name = self.pcap_file_path.split("/")[-1]
         self.pcap_file_label_variable.set("Classifying " + self.pcap_file_name)
         # Show Classifying animation
-        self.pcap_label.grid(column=2, row=5, columnspan=2)
-        self.progress_bar.grid(column=2, row=6, columnspan=2)
+        self.pcap_label.grid(column=2, row=6, columnspan=2)
+        self.progress_bar.grid(column=2, row=7, columnspan=2)
         self.progress_bar.start(15)
         # Begin classifying process
         threading.Thread(target=lambda: self.classify_pcap_file()).start()
@@ -99,29 +105,62 @@ class PcapClassificationPage(ttk.Frame):
 
     def check_classify_progress(self):
         try:
-            names, values = result_queue.get()
+            labels, x, y_axis = result_queue.get()
             self.progress_bar.grid_forget()
             self.pcap_label.grid_forget()
-            self.create_graph(names, values)
+            self.slider.grid(column=2, row=5, columnspan=2)
+            self.pcap_values = list(zip(*y_axis))
+            self.pcap_lables = labels
+            self.create_graph(labels, x, y_axis)
         except queue.Empty:
             pass
 
-    def create_graph(self, names, values):
-        p = self.figure.add_subplot(111)
+    def on_slider_change(self, value):
+        if self.slider_graph is not None:
+            x_axis = list(range(60, 360, 60))
+            self.slider_graph.clear()
+            y = self.pcap_values[x_axis.index(int(value))]
+            for label, value in zip(self.pcap_lables, y):
+                self.slider_graph.barh(label, value)
+            self.slider_graph.set_xlim(0, 1)
+            for i, v in enumerate(y):
+                self.slider_graph.text(v, i, str(v), fontweight='bold')
+
+            self.graph.draw()
+
+    def create_graph(self, labels, x, y_axis):
+        p = self.figure.add_subplot(211)
         self.figure.suptitle(self.pcap_file_name)
-        p.bar(names, values)
+        for label, y in zip(labels, y_axis):
+            p.plot(x, y, label=label)
+        p.legend()
+
+        self.slider_graph = self.figure.add_subplot(212)
+        y = self.pcap_values[0]
+        for label, value in zip(labels, y):
+            self.slider_graph.barh(label, value)
+        self.slider_graph.set_xlim(0, 1)
+        for i, v in enumerate(y):
+            self.slider_graph.text(v, i, str(v), fontweight='bold')
+
         self.graph.draw()
 
     def classify_pcap_file(self):
         # TODO change to actual classification
-        names = ['VoIP', 'Video', 'File Transfer', 'Chat', "Browsing"]
-        values = [43, 22, 15, 93, 85]
-        result_queue.put((names, values))
+        labels = ["Video", "Chat", "VoIP"]
+        x = list(range(60, 360, 60))
+        print(x)
+        flow1 = [0.2, 0.26, 0.30, 0.33, 0.4]
+        flow2 = [0.3, 0.24, 0.18, 0.2, 0.2]
+        flow3 = [0.4, 0.4, 0.5, 0.4, 0.32]
+        y_axis = [flow1, flow2, flow3]
+        result_queue.put((labels, x, y_axis))
 
     def on_back_button_click(self):
         self.progress_bar.stop()
         self.progress_bar.grid_forget()
         self.pcap_label.grid_forget()
+        self.slider.grid_forget()
         self.figure.clear()
         self.graph.draw()
         self.controller.show_frame(StartPage)
