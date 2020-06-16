6b6f5b6dcf87daea4d8261f68160e32e42981394
gilzim <gilzi@campus.technion.ac.il>
2020-06-16 16:48:46 +0300

    Gui now  presets correct graph per chosen flow

--- gui/graph_frame.py
+++ gui/graph_frame.py
@@ -20,6 +20,11 @@ import matplotlib.pyplot as plt
 result_queue = queue.Queue()
 COMPLETED = "completed"
 TIME_INTERVAL = 60
+BLOCK_INTERVAL = 2
+
+
+def _blocks_to_time(block_amount):
+    return 60 + (15 * (block_amount - 1))
 
 
 class FlowPicGraphFrame(ttk.Frame):
@@ -34,6 +39,7 @@ class FlowPicGraphFrame(ttk.Frame):
 
         self.max_time = 0
         self.min_time = 0
+        self.flows_map = {}
 
         self.title = ""
         self.figure = plt.figure(figsize=(8, 8), dpi=100)
@@ -65,7 +71,7 @@ class FlowPicGraphFrame(ttk.Frame):
             fill_values = list(zip(*y_values_for_fill[label]))
             graph.fill_between(x, fill_values[1], fill_values[0])
         graph.set_xlabel("time in seconds")
-        graph.set_ylabel("bytes per second")
+        graph.set_ylabel("Kbps")
         graph.set_xlim(x[0], x[-1])
         graph.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=len(labels))
 
@@ -88,22 +94,15 @@ class FlowPicGraphFrame(ttk.Frame):
         actual_graph.set_title("Actual Categories Bandwidth for " + self.title)
         self._create_graph(actual_graph, labels, x, y_axis)
 
-    def _create_combobox(self, flows):
-        self.flow_selection["values"] = flows
+    def _create_combobox(self):
+        self.flow_selection["values"] = list(self.flows_map.keys())
 
     def _on_flow_select(self, event):
         self.figure_per_flow.clear()
-        # TODO change from hardcoded values
-
-        labels = ["Video", "Chat", "VoIP"]
-        x = list(range(60, 360, 60))
-        flow1 = [0.2, 0.26, 0.30, 0.33, 0.4]
-        flow2 = [0.3, 0.24, 0.18, 0.2, 0.2]
-        flow3 = [0.4, 0.4, 0.5, 0.4, 0.32]
-        y_axis = [flow1, flow2, flow3]
 
         graph = self.figure_per_flow.add_subplot(111)
         flow = self.flow_selection["values"][self.flow_selection.current()]
+        labels, x, y_axis = self._extract_flow_values(self.flows_map[flow])
 
         graph.set_title("Classification for " + flow.__str__())
 
@@ -111,11 +110,13 @@ class FlowPicGraphFrame(ttk.Frame):
         self.graph_per_flow._tkcanvas.grid(column=0, row=1, columnspan=3)
         self.return_button.grid(column=0, row=0, sticky=W)
         self._create_graph(graph, labels, x, y_axis)
+        graph.legend(loc='upper center', bbox_to_anchor=(0.5, -0.07), shadow=True, ncol=len(labels))
         self.graph_per_flow.draw()
 
     def _on_return_click(self):
         self.graph_per_flow._tkcanvas.grid_forget()
         self.graph._tkcanvas.grid(column=0, row=1, columnspan=3)
+        self.flow_selection.selection_clear()
         self.return_button.grid_forget()
 
     def _extract_graph_values(self, flows_data):
@@ -133,34 +134,54 @@ class FlowPicGraphFrame(ttk.Frame):
                 if time == start_interval:
                     continue
                 sums = [np.sum(classified_flow.flow.sizes[((
-                                                                classified_flow.flow.times + classified_flow.flow.pcap_relative_start_time >= start_interval) & (
-                                                                classified_flow.flow.times + classified_flow.flow.pcap_relative_start_time < time))])
+                                                                   classified_flow.flow.times + classified_flow.flow.pcap_relative_start_time >= start_interval) & (
+                                                                   classified_flow.flow.times + classified_flow.flow.pcap_relative_start_time < time))])
                         for classified_flow in flows_by_categories]
                 flows[index].append(np.sum(sums))
                 start_interval += TIME_INTERVAL
 
-        flows = [np.array(flow) / TIME_INTERVAL for flow in flows]
+        flows = [(np.array(flow) / 1000) / TIME_INTERVAL for flow in flows]
 
         return self.categories, x, flows
+
+    def _extract_flow_values(self, classified_flow: ClassifiedFlow):
+        x = list(np.arange(BLOCK_INTERVAL, len(classified_flow.classified_blocks) + BLOCK_INTERVAL, BLOCK_INTERVAL))
+        blocks = [[] for _ in self.categories]
+        for block_amount in x:
+            sums_per_pred = [0 for _ in self.categories]
+            for index, block in enumerate(classified_flow.classified_blocks[:block_amount]):
+                sizes = np.array([data[1] for data in block.block.data])
+                times = np.array([data[0] for data in block.block.data])
+                pred = block.pred
+                if index == 0:
+                    sizes = np.sum(sizes)
+                else:
+                    sizes = np.sum(sizes[((times >= 45) & (times <= 60))])
+                sums_per_pred[pred] += sizes / 1000
+            blocks = [block+[sums / _blocks_to_time(block_amount)] for block, sums in zip(blocks, sums_per_pred)]
+        x = [_blocks_to_time(block_amount) for block_amount in x]
+
+        return self.categories, x, blocks
 
     def classify_pcap_file(self, filepath):
         self.title = filepath.split("/")[-1]
         flows_data = self.classifier.classify_file(Path(filepath))
 
         labels, x, y_axis = self._extract_graph_values(flows_data)
-        flows = [classified_flow.flow.five_tuple for classified_flow in itertools.chain.from_iterable(flows_data)]
+        self.flows_map = {classified_flow.flow.five_tuple.__str__(): classified_flow for classified_flow in
+                          itertools.chain.from_iterable(flows_data)}
 
-        self._create_combobox(flows)
+        self._create_combobox()
         self._generate_predicted_graph(labels, x, y_axis)
         self._generate_actual_graph(filepath)
         result_queue.put(COMPLETED)
 
     def clear_graphs(self):
+        self._on_return_click()
         self.figure.clear()
-        self.figure_per_flow.clear()
-        self.graph_per_flow.draw()
         self.graph.draw()
-        self.return_button.grid_forget()
+        # self.figure_per_flow.clear()
+        # self.graph_per_flow.draw()
         self.flow_selection_label.grid_forget()
         self.flow_selection.grid_forget()
 
