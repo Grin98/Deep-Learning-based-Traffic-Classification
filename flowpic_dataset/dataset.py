17bf76bfc98d7503d1a8793a1fa4e3f64bd77fed
Grin98 <norgrin123@gmail.com>
2020-07-11 12:06:24 +0300

    added comments to utils.py

--- flowpic_dataset/dataset.py
+++ flowpic_dataset/dataset.py
@@ -9,7 +9,7 @@ from torch.utils.data.dataset import Dataset
 import csv
 from typing import List, Sequence, Tuple, NamedTuple, overload
 import numpy as np
-from flowpic_dataset.processors import QuickFlowFileProcessor, BasicProcessor
+from flowpic_dataset.processors import BasicProcessor
 from misc.data_classes import Flow, Block
 from misc.utils import build_pic
 
