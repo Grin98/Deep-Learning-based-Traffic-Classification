2d89dc1b93fe14896c963c76893c4ea30e08c655
Grin98 <norgrin123@gmail.com>
2020-08-12 12:48:06 +0300

    fixed speeling mistake in requirments.txt and added activation of flowpic environment in script to install the packges there and not in base

--- environment_setup/create_enviroment.sh
+++ environment_setup/create_enviroment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 conda env create -f environment.yml
+conda activate flowpic
 pip install torch===1.5.1 torchvision===0.6.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
